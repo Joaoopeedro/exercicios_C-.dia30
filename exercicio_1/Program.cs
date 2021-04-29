8e1f480c0fbca210509bc056ecaa125d183cbd76
joao <joaopedro37@gmail.com>
2021-04-29 17:32:02 -0300

    exercicios finalizados

---
+++ exercicio_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Qual é seu nome ?");
+            string nomePessoa = Console.ReadLine();
+
+            Console.WriteLine("Boa tarde " + nomePessoa + "!!" + "\n");
+
+            Console.WriteLine("Quantos anos você tem?");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int meses = idade * 12;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(meses + " :meses" );
+
+            int anobissexto = idade * 366;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(anobissexto + " :dias em ano bissexto");
+            
+            int dias = idade * 365;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(dias + " :dias" );
+            
+            int horas = dias * 365;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(horas + " :horas");
+
+            int minutos = horas * 60;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(minutos + " :min");
+
+            
+        }
+    }
+}
---
+++ exercicio_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("exercicio_2");
+            Console.WriteLine("Qual o ano do seu nascimento?");
+
+            int ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual ano estamos?");
+            int anoAtual = int.Parse(Console.ReadLine());
+
+            int idade = anoAtual - ano;
+            Console.WriteLine("Você tem a idade de: " + idade + " anos");
+
+            int idadeSemana = idade * 12 * 4;
+            Console.WriteLine("Sua idade convertida em semanas é de: " +  idadeSemana + " semanas");
+
+        }
+    }
+}
